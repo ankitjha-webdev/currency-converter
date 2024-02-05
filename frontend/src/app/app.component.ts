d3b9621101914dda0cca9aac53cf9814e21e5095
ankitjha-webdev <190101120091@cutm.ac.in>
2024-02-06 01:44:18 +0530

    file changes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,50 +4,71 @@ import { RouterOutlet } from '@angular/router';
 import { CurrencyService } from './currency.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HttpClientModule,
-    FormsModule,
-    CommonModule
-  ],
+  imports: [RouterOutlet, HttpClientModule, FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'frontend';
   _http = inject(HttpClient);
   private API_KEY = '86c4376903aa446e9c63fc2024197506';
   private apiUrl = `https://openexchangerates.org/api/latest.json?app_id=${this.API_KEY}`;
-  amount: number = 1;
-  fromCurrency: string = 'USD';
-  toCurrency: string = 'EUR';
-  result!: number | undefined;
+  amount: number = 100;
+  fromCurrency: string = 'GBP';
+  toCurrency: string = 'USD';
+  result: any;
+  exchanges: any;
+  exchangeRates: any;
+  currentTimeInUK = new Date().toLocaleTimeString('en-GB', {
+    timeZone: 'Europe/London',
+    hour12: true,
+  });
 
   constructor(private currencyService: CurrencyService) {
-    this.getLatestExchangeRates()
+    this.getLatestExchangeRates();
   }
 
-  getLatestExchangeRates(): Observable<any> {
-    return this._http.get<any>(this.apiUrl);
+  getLatestExchangeRates(): void {
+    this._http.get<any>(this.apiUrl).subscribe({
+      next: (response: any) => {
+        this.exchanges = response;
+        this.exchangeRates = Object.keys(this.exchanges.rates).map(
+          (currency) => ({
+            currency: currency,
+            rate: this.exchanges.rates[currency],
+          })
+        );
+        this.convertCurrency();
+      },
+      error(err) {
+        console.log(err);
+      },
+    });
   }
 
   convertCurrency() {
-    return this.getLatestExchangeRates().pipe(map((response: any) => {
-      const fromRate = response.rates[this.fromCurrency];
-      const toRate = response.rates[this.toCurrency];
+    console.log(this.exchanges);
+    console.log(
+      this.fromCurrency,
+      this.toCurrency,
+      'this.fromCurrency | this.fromCurrency',
+      this.exchanges.rates
+    );
 
-      if (fromRate !== undefined && toRate !== undefined) {
-        this.result = (this.amount / fromRate) * toRate;
-      } else {
-        this.result = undefined;
-      }
-      // amount: number, fromCurrency: string, toCurrency: string
-    }));
+    const fromRate = this.exchanges.rates[this.fromCurrency];
+    const toRate = this.exchanges.rates[this.toCurrency];
+
+    if (fromRate && toRate) {
+      this.result = ((this.amount / fromRate) * toRate).toFixed(2);
+    } else {
+      console.log(this.result, 'dsfdghfg999999999');
+
+      this.result = undefined;
+    }
+    // amount: number, fromCurrency: string, toCurrency: string
   }
 }
--- frontend/src/app/currency.service.ts
+++ frontend/src/app/currency.service.ts
@@ -1,11 +1,11 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class CurrencyService {
-  private http: inject(HttpClient)
+  // private http: inject(HttpClient)
   private API_KEY = '86c4376903aa446e9c63fc2024197506';
   private apiUrl = `https://openexchangerates.org/api/latest.json?app_id=${this.API_KEY}`;
   private exchangeRates: { [key: string]: number } = {
@@ -17,20 +17,20 @@ export class CurrencyService {
 
   constructor() { }
 
-  getLatestExchangeRates(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
-  }
+  // getLatestExchangeRates(): Observable<any> {
+  //   return this.http.get<any>(this.apiUrl);
+  // }
 
-  convertCurrency(amount: number, fromCurrency: string, toCurrency: string): Observable<number | undefined> {
-    return this.getLatestExchangeRates().pipe(map((response:any) => {
-      const fromRate = response.rates[fromCurrency];
-      const toRate = response.rates[toCurrency];
+  // convertCurrency(amount: number, fromCurrency: string, toCurrency: string): Observable<number | undefined> {
+  //   return this.getLatestExchangeRates().pipe(map((response:any) => {
+  //     const fromRate = response.rates[fromCurrency];
+  //     const toRate = response.rates[toCurrency];
 
-      if (fromRate !== undefined && toRate !== undefined) {
-        return (amount / fromRate) * toRate;
-      } else {
-        return undefined;
-      }
-    }));
-  }
+  //     if (fromRate !== undefined && toRate !== undefined) {
+  //       return (amount / fromRate) * toRate;
+  //     } else {
+  //       return undefined;
+  //     }
+  //   }));
+  // }
 }
